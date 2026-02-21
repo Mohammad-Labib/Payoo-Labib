a6101380d35888c01cb3baa553b767ca0cfafaea
Mohammad-Labib <friendlabib@gmail.com>
2026-02-21 10:49:53 +0600

    finxd login link

--- script/login.js
+++ script/login.js
@@ -13,7 +13,7 @@ document.getElementById("login-btn").addEventListener("click", function(){
         //true> alert > homepage
         alert("login Success")
         // window.location.replace("/home.html")
-         window.location.assign("/home.html");
+         window.location.assign("home.html");
 
     }else{
         // true > alert return
